985fb9bc34dc894b1089dc62d0c6a9b4c37c6ce8
Masayuki Ishikawa <masayuki0608ishikawa@gmail.com>
2026-06-03 22:14:30 +0900

    ALUモジュールの追加と演算処理の実装

---
+++ alu/alu.v
@@ -0,0 +1,19 @@
+module alu(
+    input[3:0] alu_operatio,
+    input[63:0] input_data1,
+    input[63:0] input_data2,
+    output reg  [63:0] alu_result,
+    output zero);
+
+    always @(*)
+    case (ale_opration)
+        4'b0000 : alu_result = input_data1 & input_data2 //`ALU_OP_AND
+        4'b0001 : alu_result = input_data1 | input_data2 //`ALU_OP_OR
+        4'b0010 : alu_result = input_data1 + input_data2 //`ALU_OP_ADD
+        4b'0110 : alu_result = input_data1 - input_data2//`ALU_OP_SUB
+        default : alu_result = 0;
+    endcase
+
+    assign zero = (alu_result == 64'b0);
+
+endmodule
